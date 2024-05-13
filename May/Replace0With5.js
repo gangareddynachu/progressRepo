dd3f1940816af9e6dea69a194aa6f1ee1fa6f6dd
Gangareddy Nachu <gnachu@horizon.csueastbay.edu>
2024-05-13 08:56:04 -0700

    Update Replace0With5.js

--- May/Replace0With5.js
+++ May/Replace0With5.js
@@ -1,3 +1,17 @@
+/*Approach:
+Use the concept of 10^0 for units place
+10^1 for tens place
+10^2 for 100's place and so on....
+
+get the digit at that place: by doing number%10
+if digit is 0 make it 5
+then have a holder variable known as ans, starting with 0
+
+do 
+ans = ans + Math.pow(10,i++) * rem; 
+
+*/
+
 
 import answerValidator from "./GeneralUtil.js";
 function convertZeroDigitstoFive(number){
